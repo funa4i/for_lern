d3f952848580b94bbbc188689af42073757cb5cc
funa4i <lyhov.tim09@gmail.com>
2022-02-03 20:11:00 +0300

    dude

--- main.py
+++ main.py
@@ -21,5 +21,29 @@ def image_mars():
                         </body>
                     </html>'''
 
+
+@app.route("/promotion_image")
+def protmotion_image():
+    return f'''<!doctype html>
+                <html lang="ru">
+                    <head>
+                        <meta charset="utf-8">
+                        <title>Привет, Марс</title>
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    </head>
+                    <body>  
+                        <h1>Жди на Марс!</h1>' 
+                        <img src="{url_for('static', filename='img/scale_1200.png')}" 
+                                alt="вот она какая красная планета"> 
+                        <p>Человечество вырастает из детства.</p>
+                        <div class="alert alert-success" role="alert"> Человечеству мала одн планета 
+                        </div>
+                    </body>
+                </html>'''
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
