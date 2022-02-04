be822840866cb433d9a3cc12093d5fe3a96fb9cd
funa4i <lyhov.tim09@gmail.com>
2022-02-04 10:36:55 +0300

    dude

--- main.py
+++ main.py
@@ -33,15 +33,17 @@ def protmotion_image():
                         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
                         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
                         crossorigin="anonymous">
-                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
                     </head>
                     <body>  
                         <h1>Жди на Марс!</h1>' 
                         <img src="{url_for('static', filename='img/scale_1200.png')}" 
                                 alt="вот она какая красная планета"> 
                         <p>Человечество вырастает из детства.</p>
-                        <div class="alert alert-success" role="alert"> Человечеству мала одн планета 
-                        </div>
+                        <div class="alert alert-success" role="alert"> Человечеству мала одна планета! </div>
+                        <p2>Мы сделаем обитаемыми безжизненые пока планеты!</p2>
+                        <div class="alert alert-warning" role="alert"> И начнем с марса! </div>
+                        <div class="alert alert-danger" role="alert"> Присоеденяйся! </div>
                     </body>
                 </html>'''
 
